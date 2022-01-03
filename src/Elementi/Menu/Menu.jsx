b7fbc0e6d92c5c4fc556cec1578f7349c5b8e3e8
Stefke Velja <stef.fac150@gmail.com>
2022-01-03 18:38:39 +0100

    Zavrsen menu

--- src/Elementi/Menu/Menu.jsx
+++ src/Elementi/Menu/Menu.jsx
@@ -10,62 +10,69 @@ import Pocetna from "./../../Stranice/Pocetna/Pocetna";
 import Kontakt from "./../../Stranice/Kontakt/Kontakt";
 import Kategorije from "./../../Stranice/Kategorije/Kategorije";
 import Novosti from "./../../Stranice/Novosti/Novosti";
-function Menu() {
+class Menu extends React.Component{
     //koristi se ComponentDidMount() funkcija - class komponenta je (napokon)
-    var list=[];
-    list = document.querySelectorAll('li.list');
-    console.log(list);
-    function activeLink(){
-        list.forEach((item)=>item.classList.remove('active'));
+    componentDidMount(){ 
+        var list1=[];
+        list1=document.querySelectorAll('.list');
+        console.log(list1);
+        list1.forEach((item)=>item.addEventListener('click',this.activeLink));
+    }
+    activeLink(){
+        var list1=[];
+        list1=document.querySelectorAll('.list');
+        list1.forEach((item)=>item.classList.remove('active'));
         this.classList.add('active');
     }
-    list.forEach((item)=>item.addEventListener('click',activeLink));
-    return (
-        <Router>
-            <nav >
-                <ul>
-                    <li className="list active">
-                        <Link to="/" >
-                        <span className='icon'>
-                            <IoMdHome/>
-                        </span>
-                        <span className='text'>Poctna</span>
-                        </Link>
-                    </li>
-                    <li className="list">
-                        <Link to="/kategorije">
-                        <span className='icon'>
-                            <IoIosApps/>
-                        </span>
-                        <span className='text'>Kategorije</span>
-                        </Link>
-                    </li>
-                    <li className="list">
-                        <Link to="/novosti">
-                        <span className='icon'>
-                            <IoMdPaper/>
-                        </span>
-                        <span className='text'>Novosti</span>
-                        </Link>
-                    </li>
-                    <li className="list">
-                        <Link to="/kontakt">
-                        <span className='icon'>
-                            <IoIosCall/>
-                        </span>
-                        <span className='text'>Kontakt</span>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route exact path="/" element={<Pocetna/>}/>
-                <Route exact path="/kategorije" element={<Kategorije/>}/>
-                <Route exact path="/novosti" element={<Novosti/>}/>
-                <Route exact path="/kontakt" element={<Kontakt/>}/>
-            </Routes>
-        </Router>
-    )
+    render()
+    {
+        return (
+            <Router>
+                <nav >
+                    <ul>
+                        <li className="list active">
+                            <Link to="/" >
+                            <span className='icon'>
+                                <IoMdHome/>
+                            </span>
+                            <span className='text'>Poctna</span>
+                            </Link>
+                        </li>
+                        <li className="list">
+                            <Link to="/kategorije">
+                            <span className='icon'>
+                                <IoIosApps/>
+                            </span>
+                            <span className='text'>Kategorije</span>
+                            </Link>
+                        </li>
+                        <li className="list">
+                            <Link to="/novosti">
+                            <span className='icon'>
+                                <IoMdPaper/>
+                            </span>
+                            <span className='text'>Novosti</span>
+                            </Link>
+                        </li>
+                        <li className="list">
+                            <Link to="/kontakt">
+                            <span className='icon'>
+                                <IoIosCall/>
+                            </span>
+                            <span className='text'>Kontakt</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route exact path="/" element={<Pocetna/>}/>
+                    <Route exact path="/kategorije" element={<Kategorije/>}/>
+                    <Route exact path="/novosti" element={<Novosti/>}/>
+                    <Route exact path="/kontakt" element={<Kontakt/>}/>
+                </Routes>
+            </Router>
+        )
+    }
 }
 
-export default Menu
+export default Menu;
